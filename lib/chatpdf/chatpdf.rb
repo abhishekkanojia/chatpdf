563911a88fe80a72a43087b0fca68407ed1b81a2
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-28 12:54:31 +0530

    Update order

--- lib/chatpdf/chatpdf.rb
+++ lib/chatpdf/chatpdf.rb
@@ -1,11 +1,12 @@
-require 'chatpdf/version'
 require 'chatpdf/client'
+require 'chatpdf/version'
 require 'chatpdf/api/file_upload'
-
 require 'chatpdf/api/base'
 require 'chatpdf/response_handler'
 
 Gem.find_files("chatpdf/**/*.rb").each { |path| require_relative path }
+
+require 'chatpdf/response_handler'
 
 module Chatpdf
 end
