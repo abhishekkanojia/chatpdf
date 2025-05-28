ab28c50ced62161e9243b2aad0f1c125cdc1bbba
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-28 15:17:58 +0530

    Provide correct file name

--- lib/chatpdf/api/file_upload.rb
+++ lib/chatpdf/api/file_upload.rb
@@ -1,6 +1,6 @@
 module Chatpdf
   module Api
-    class FileUpload < Base
+    class FileUpload < Chatpdf::Api::Base
       def add_pdf_via_upload(file_path)
         file = File.new(file_path)
         response = client.request("/sources/add-file", {
