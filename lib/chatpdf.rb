55739a25f3d6139e2034f65d4952ac83963d60a0
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-28 16:57:44 +0530

    Remove unused class

--- lib/chatpdf.rb
+++ lib/chatpdf.rb
@@ -2,8 +2,10 @@
 
 require 'chatpdf/client'
 require 'chatpdf/version'
-require 'chatpdf/api/file_upload'
+
 require 'chatpdf/api/base'
+require 'chatpdf/api/file_upload'
+
 require 'chatpdf/response_handler'
 
 Gem.find_files("chatpdf/**/*.rb").each { |path| require_relative path }
--- lib/chatpdf/api/base.rb
+++ lib/chatpdf/api/base.rb
@@ -1,6 +1,11 @@
 module Chatpdf
   module Api
     class Base
+      attr_reader :client
+      
+      def initialize
+        @client = Chatpdf::Client.new
+      end
     end
   end
 end
