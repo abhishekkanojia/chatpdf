f4d5453581fb3771ac8d0c5247a1ae250972f0fa
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-28 16:55:30 +0530

    Remove code from base

--- lib/chatpdf/api/base.rb
+++ lib/chatpdf/api/base.rb
@@ -1,11 +1,6 @@
 module Chatpdf
   module Api
     class Base
-      attr_reader :client
-      
-      def initialize
-        @client = Chatpdf::Client.new
-      end
     end
   end
 end
--- lib/chatpdf/api/file_upload.rb
+++ lib/chatpdf/api/file_upload.rb
@@ -1,6 +1,12 @@
 module Chatpdf
   module Api
     class FileUpload < Chatpdf::Api::Base
+      attr_reader :client
+      
+      def initialize
+        @client = Chatpdf::Client.new
+      end
+
       def add_pdf_via_upload(file_path)
         file = File.new(file_path)
         response = client.request("/sources/add-file", {
