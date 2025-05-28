05c2992aa9a956bed271f397e0a8668a72129c46
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-28 12:08:08 +0530

    Update chatpdf

--- lib/chatpdf/chatpdf.rb
+++ lib/chatpdf/chatpdf.rb
@@ -5,5 +5,7 @@ require 'chatpdf/api/file_upload'
 require 'chatpdf/api/base'
 require 'chatpdf/response_handler'
 
+Gem.find_files("chatpdf/**/*.rb").each { |path| require_relative path }
+
 module Chatpdf
 end
