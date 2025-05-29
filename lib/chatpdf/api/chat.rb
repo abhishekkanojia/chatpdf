ff97eba63c050e5d352de078c028746773ab2eb7
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-29 14:41:44 +0530

    Add chat with reference method

--- lib/chatpdf/api/chat.rb
+++ lib/chatpdf/api/chat.rb
@@ -11,6 +11,17 @@ module Chatpdf
 
         ResponseHandler.new(response).response_body
       end
+
+      def chat_with_reference(source_id, messages = [])
+        response = client.request("/chats/message", {
+          body: {
+            referenceSources: true,
+            sourceId: source_id,
+            messages: messages
+          }
+        })
+        ResponseHandler.new(response).response_body
+      end
     end
   end
 end
