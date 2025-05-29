f1e941e6c3b2eabe047b0b15b7a0da7fd76dd591
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-29 13:25:04 +0530

    Add rb for file

--- lib/chatpdf/api/file_upload.rb
+++ lib/chatpdf/api/file_upload.rb
@@ -2,7 +2,7 @@ module Chatpdf
   module Api
     class FileUpload < Chatpdf::Api::Base
       def add_pdf_via_upload(file_path)
-        file = File.new(file_path)
+        file = File.new(file_path ,'rb')
         response = client.request("/sources/add-file", {
           body: {
             file: file
