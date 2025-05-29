88c3593c83a9c5fde7436183f8a37c633ad82bc2
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-29 19:02:35 +0530

    Add action add_pdf_via_url to client

--- lib/chatpdf/concerns/actionable.rb
+++ lib/chatpdf/concerns/actionable.rb
@@ -5,6 +5,10 @@ module Chatpdf
         Chatpdf::Api::FileUpload.new(self).add_pdf_via_upload(file_path)
       end
 
+      def add_pdf_via_url(url)
+        Chatpdf::Api::FileUpload.new(self).add_pdf_via_url(url)
+      end
+
       def chat(source_id, messages)
         Chatpdf::Api::Chat.new(self).chat(
           source_id,
