1ee948ea553c6375599af170e884fc5b827c312e
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-29 12:36:26 +0530

    Fix

--- lib/chatpdf/message.rb
+++ lib/chatpdf/message.rb
@@ -1,15 +1,20 @@
 module Chatpdf
   class Message
-    def initialize(content, role = "user")
+    def initialize(content, role: "user", stream: false)
       @content = content
       @role = role
+      @stream = stream
     end
 
     def to_h
-      {
+      default = {
         role: @role,
-        content: @content
+        content: @content,
       }
+      
+      default.merge(stream: @stream) if @stream
+
+      default
     end
   end
 end
