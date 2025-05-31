271b1d358f2241c2462a85f637e20399dd871ffb
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-05-31 23:35:16 +0530

    Add references method

--- lib/chatpdf/session/answer.rb
+++ lib/chatpdf/session/answer.rb
@@ -1,9 +1,10 @@
 module Chatpdf
   module Session
     class Answer
+      PAGE_NUMBER_KEY = "pageNumber".freeze
       @@_id = 0
 
-      attr_reader :id, :content, :references
+      attr_reader :id, :content
 
       def initialize(response = {})
         @content = response["content"]
@@ -14,6 +15,10 @@ module Chatpdf
 
       def to_s
         @content
+      end
+
+      def references
+        @references.map { |reference| reference[PAGE_NUMBER_KEY] }
       end
     end
   end
