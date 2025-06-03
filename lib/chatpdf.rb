729da16276267e34499d66355310ca5972f912a0
Abhishek Kanojia <abhishekka3193@gmail.com>
2025-06-03 14:19:30 +0530

    Fix loading issue

--- lib/chatpdf.rb
+++ lib/chatpdf.rb
@@ -1,34 +1,27 @@
 # frozen_string_literal: true
 
+require 'chatpdf/version'
 require 'chatpdf/configuration'
 require 'chatpdf/concerns/actionable'
-
 require 'chatpdf/client'
-require 'chatpdf/version'
-
 require 'chatpdf/api/base'
 require 'chatpdf/api/file_upload'
 require 'chatpdf/api/chat'
 require 'chatpdf/api/delete_source'
 require 'chatpdf/api/chat_stream'
-
 require 'chatpdf/exceptions/invalid_session_initialization'
 require 'chatpdf/exceptions/invalid_configuration'
-
 require 'chatpdf/session/question'
 require 'chatpdf/session/answer'
 require 'chatpdf/session/conversation'
 require 'chatpdf/message'
-
 require 'chatpdf/response_handler'
 
-Gem.find_files("chatpdf/**/*.rb").each { |path| require_relative path }
 begin
   require "chatpdf/railtie" if defined?(Rails::Railtie)
 rescue LoadError
   # Rails is not present
 end
-
 
 module Chatpdf
   class << self
@@ -37,6 +30,7 @@ module Chatpdf
 
   def self.configure
     self.configuration ||= Configuration.new
-    yield(configuration)
+    yield(configuration) if block_given?
+    configuration
   end
 end
